460e74962a0c0d842894684bab1df781694a0d86
Eliam Villegas <102682631+eliam-villegas@users.noreply.github.com>
2025-08-06 15:07:58 -0400

    Implementa controlador basico de tasks Se aplican metodos basico con get y post para aprender logica de nest y typescript.

--- apps/api/src/tasks/tasks.controller.ts
+++ apps/api/src/tasks/tasks.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+
+    @Get()
+    findAll(){
+        return [ {id: 1, title: "Aprender NestJS", done: false},
+                 {id: 2, title: "Crear Endpoint", done: true},
+        ];
+    }
+
+    @Post()
+    create(@Body() task: any){
+        return {
+            message: "Tarea recibida",
+            data: task,
+        };
+    }
+
+}
